3878f34c40db103f72593c77670dcb492cd291c0
Lucas Levi <lucas.lsl12@gmail.com>
2021-02-11 22:15:09 -0300

    Implement clone Interface

--- ViewModel/PacienteViewModel.cs
+++ ViewModel/PacienteViewModel.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Cadastro_de_Pessoas.Model; // to import the folder where my classes are
+using  Cadastro_de_Pessoas.Model; // to import the folder where my classes are
 using Cadastro_de_Pessoas.ViewModel.Commands;
 
-namespace Cadastro_de_Pessoas.ViewModel
+namespace Cadastro_de_Pessoas.ViewModel 
 {
     public class PacienteViewModel : BaseNotifyPropertyChanged
     {
-        public System.Collections.ObjectModel.ObservableCollection<Model.Paciente> Pacientes { get; private set; }
+        public ObservableCollection<Model.Paciente> Pacientes { get; private set; }
         public DeletarCommand Delete { get; private set; } = new DeletarCommand();
 
         public NovoCommand New { get; private set; } = new NovoCommand();
@@ -18,7 +19,11 @@ namespace Cadastro_de_Pessoas.ViewModel
         private Model.Paciente _selectedPacient; // we use _ when some attribute is private
         public Model.Paciente SelectedPacient
         {
-            get { return _selectedPacient; }
+            get 
+            {
+
+                return _selectedPacient;
+            }
             set 
             { 
                 SetField(ref _selectedPacient, value);
@@ -28,18 +33,7 @@ namespace Cadastro_de_Pessoas.ViewModel
     
         public PacienteViewModel()
         {
-            Pacientes = new System.Collections.ObjectModel.ObservableCollection<Model.Paciente>();
-            Pacientes.Add(new Model.Paciente() // testing to add some value on my screen
-            {
-
-                Id = 1,
-                name = "Lucas",
-                lastName = "Levi",
-                birth = new DateTime(1998, 12, 07),
-                sex = Model.Sexo.Masculino,
-                martial = Model.EstadoCivil.Casado,
-                diagnosisDate = new DateTime(2021, 2, 10)
-            });
+            Pacientes = new ObservableCollection<Model.Paciente>();
 
             SelectedPacient = Pacientes.FirstOrDefault(); // one of properties, to show the first register pacient or some default value
         }
